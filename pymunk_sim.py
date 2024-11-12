3059989e4d0cff3adf5d4f6895d09374c639de21
efekaanefe <efekaanefe1905@gmail.com>
2024-11-12 20:17:26 +0300

    pymunk initial setup

--- pymunk_sim.py
+++ pymunk_sim.py
@@ -1,5 +1,6 @@
 import pymunk
 import pygame
+import pymunk.pygame_util
 
 pygame.init()
 
@@ -8,19 +9,28 @@ HEIGHT = 600
 
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 
+def draw(space, window, draw_options):
+    window.fill("white")
+    space.debug_draw(draw_options)
+
 def run(window, width, height):
     run = True
     clock = pygame.time.Clock()
-    FPS = 60
+    FPS = 75
+    dt = 1/FPS
 
     space = pymunk.Space() # where we put objects    
-    space.gravity = (0, -9.81)
+    space.gravity = (0, 9.81)
+
+    draw_options = pymunk.pygame_util.DrawOptions(window)
 
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 break
+        draw(space, window, draw_options)
+        space.step(dt)
 
         clock.tick(FPS)
     
