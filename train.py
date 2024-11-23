6da17eeb9bf3d8c86931dac4a9ee51a4bcaed855
efekaanefe <efekaanefe1905@gmail.com>
2024-11-23 18:49:55 +0300

    pid agent not working yet

--- train.py
+++ train.py
@@ -1,6 +1,6 @@
 import numpy as np
 from inverted_pendulum_env import InvertedPendulumEnv
-
+from agents import PIDAgent
 
 if __name__ == "__main__":
     env = InvertedPendulumEnv(
@@ -11,14 +11,22 @@ if __name__ == "__main__":
         base_mass=5,
         link_size=(6, 250), 
         link_mass=2,
+        groove_length = 600,
         max_angle=np.pi/3, 
         max_position=500,
-        max_steps = 1000)
+        max_steps = 100)
+
+    obs_goal = np.array([env.groove_length/2, 0, np.pi/2, 0])
 
     obs = env.reset()
 
+    P, I, D = 15, 5, 100 
+    agent = PIDAgent(P, I, D)
+
     for _ in range(1000):
-        action = env.action_space.sample()  # Replace with a trained policy for better control
+        obs_error = np.mean(obs_goal - obs)
+
+        action = agent.choose_action(obs_error)  # Replace with a trained policy for better control
         obs, reward, done, info = env.step(action)
         
         # debug
